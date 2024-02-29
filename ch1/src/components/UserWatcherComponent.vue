6bcb717f4677ae938dde5d95a454d35b6b25acb7
Tona-Mangobyte <tona@mango-byte.com>
2024-02-29 21:52:35 +0700

    simple

---
+++ ch1/src/components/UserWatcherComponent.vue
@@ -0,0 +1,45 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+
+type User = {
+  name: string;
+  age: number;
+};
+
+export default defineComponent({
+  name: "UserWatcherComponent",
+  data(): { user: User } {
+    return {
+      user: {
+        name: "John",
+        age: 30,
+      },
+    };
+  },
+  watch: {
+    user: {
+      handler(newValue: User, oldValue: User) {
+        console.log({ newValue, oldValue });
+      },
+      deep: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <div>
+      <label for="name"
+        >Name:
+        <input v-model="user.name" placeholder="Enter your name" id="name" />
+      </label>
+    </div>
+    <div>
+      <label for="age"
+        >Age:
+        <input v-model="user.age" placeholder="Enter your age" id="age" />
+      </label>
+    </div>
+  </div>
+</template>
--- ch1/src/views/HomeView.vue
+++ ch1/src/views/HomeView.vue
@@ -1,17 +1,17 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <UserWatcherComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
-import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+import UserWatcherComponent from "@/components/UserWatcherComponent.vue"; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    UserWatcherComponent,
   },
 })
 export default class HomeView extends Vue {}
