cfccca1b5f3838b7e5a69c56d7d20cf001b64829
Tona-Mangobyte <tona@mango-byte.com>
2024-03-01 13:20:55 +0700

    simple mixins

---
+++ ch1/src/mixins/restaurantMixin.ts
@@ -0,0 +1,24 @@
+/** mixins/restaurantMixin.ts */
+import { defineComponent } from "vue";
+
+export const restaurantMixin = defineComponent({
+  data() {
+    return {
+      menu: [],
+      reservations: [],
+      payments: [],
+      title: "Restaurant",
+    };
+  },
+  methods: {
+    makeReservation() {
+      console.log("Reservation made");
+    },
+    acceptPayment() {
+      console.log("Payment accepted");
+    },
+  },
+  created() {
+    console.log(`Welcome to ${this.title}`);
+  },
+});
--- ch1/src/views/HomeView.vue
+++ ch1/src/views/HomeView.vue
@@ -2,16 +2,19 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
     <UserWatcherComponent />
+    <CafeComponent />
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from "vue-class-component";
 import UserWatcherComponent from "@/components/UserWatcherComponent.vue"; // @ is an alias to /src
+import CafeComponent from "@/components/CafeComponent.vue";
 
 @Options({
   components: {
     UserWatcherComponent,
+    CafeComponent,
   },
 })
 export default class HomeView extends Vue {}
