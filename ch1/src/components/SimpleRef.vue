aed951ba8a622fe258f587f6bc5a59f9cb9aa247
Tona-Mangobyte <tona@mango-byte.com>
2024-03-01 11:59:19 +0700

    simple ref

---
+++ ch1/src/components/SimpleRef.vue
@@ -0,0 +1,30 @@
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "SimpleRef",
+  mounted() {
+    (this.$refs.messageRef as HTMLInputElement).focus();
+  },
+  watch: {
+    /*["$refs.messageRef"](el: HTMLInputElement) {
+      console.log("watch", el);
+    },*/
+  },
+  methods: {
+    simpleChange(event: Event) {
+      console.log((event.target as HTMLInputElement).value);
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <input
+      type="text"
+      ref="messageRef"
+      placeholder="Enter a message"
+      @change="this.simpleChange($event)"
+    />
+  </div>
+</template>
---
+++ ch1/src/views/RefsView.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+import { Options, Vue } from "vue-class-component";
+import SimpleRef from "@/components/SimpleRef.vue";
+@Options({
+  components: {
+    SimpleRef,
+  },
+})
+export default class RefsView extends Vue {}
+</script>
+
+<template>
+  <h2>Hello Refs</h2>
+  <SimpleRef />
+</template>
