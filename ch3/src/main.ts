fe37ca3041ef642118f0dcf326e2dfd720ceca19
Tona-Mangobyte <tona@mango-byte.com>
2024-03-19 11:34:04 +0700

    make simple

--- ch3/src/main.ts
+++ ch3/src/main.ts
@@ -2,4 +2,8 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router)
+// app._context.config.warnRecursiveComputed = true
+app.mount('#app')
+// createApp(App).use(router).mount('#app')
---
+++ ch3/src/views/CounterView.vue
@@ -0,0 +1,24 @@
+<template>
+  <h2>Hello watchEffect! {{ count }}</h2>
+  <h3>Total: {{ total }}</h3>
+  <button @click="increment">Click</button>
+</template>
+
+<script setup lang="ts">
+import { ref, watchEffect, watch, computed } from 'vue'
+const count = ref<number>(0)
+
+const total = computed<number>(() => count.value * 2)
+
+watchEffect(() => {
+  console.log('watchEffect ', count.value)
+})
+const watchCounter = (value: number) => {
+  console.log('watch ', value)
+}
+watch<number, boolean>(count, watchCounter)
+
+const increment = () => {
+  count.value++
+}
+</script>
